f420f3e241c9dfc0dabc8db81159da7d04ca5ca8
aomine <aomine1745@gmail.com>
2024-06-28 00:30:28 -0500

    add all styles for seats and define sets structure

--- src/main/java/aomine/view/SeatView.java
+++ src/main/java/aomine/view/SeatView.java
@@ -1,6 +1,7 @@
 package aomine.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
@@ -13,7 +14,7 @@ public class SeatView {
   @FXML
   private Text tRoomNumber;
 
-  @FXML
+  @FXML 
   private FlowPane fpSeatList;
 
   public void initialize() {
@@ -21,10 +22,17 @@ public class SeatView {
     SeatService seatService = new SeatService();
     Seat seat = seatService.get(seatId);
     String[][] seats = seat.getSeats();
-    
+    tRoomNumber.setText(seat.getRoomNumber() + "");
+
     for (int i = 0; i < seats.length; i++) {
       for (int j = 0; j < seats[i].length; j++) {
-        Button button = new Button(i + "" + j);
+        Button button = new Button("");
+        button.getStyleClass().add("seat");
+
+        if (seats[i][j].equals("V")) button.getStyleClass().add("seat-void");
+        else if (seats[i][j].equals("E")) button.getStyleClass().add("seat-empty"); 
+        else if (seats[i][j].equals("O")) button.getStyleClass().add("seat-occupied");
+
         fpSeatList.getChildren().add(button);
       }
     }
